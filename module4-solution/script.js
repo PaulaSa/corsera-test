78f2c01951ebef1090438d8555fb058a3f595224
Paula <paula.sudakova@gmail.com>
2017-01-30 19:34:55 +0100

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -33,9 +33,9 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-
+(function (window) {
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-(function (names) {
+
 // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
@@ -65,5 +65,4 @@ for (var i = 0; i < names.length; i++) {
   }
   } 
 
-names.byeSpeaker = byeSpeaker;
-})(names);
+})(window);
