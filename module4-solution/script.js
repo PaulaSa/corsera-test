32f416120f1e570ae737c6ca239bb8caa9ec9a22
Paula <paula.sudakova@gmail.com>
2017-01-30 19:29:20 +0100

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -32,10 +32,10 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function (window) {
+
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
+(function (names) {
 // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
@@ -65,5 +65,5 @@ for (var i = 0; i < names.length; i++) {
   }
   } 
 
-  window.byeSpeaker = byeSpeaker;
-})(window);
+names.byeSpeaker = byeSpeaker;
+})(names);
