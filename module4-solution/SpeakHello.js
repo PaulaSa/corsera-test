c740caa857185f6a7066a52fc4cbcfee5c8af1c6
Paula <paula.sudakova@gmail.com>
2017-01-30 19:31:23 +0100

    Update SpeakHello.js

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -10,7 +10,7 @@
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
  var helloSpeaker = {};
-helloSpeaker = "names";
+
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 var speakWord = "Hello";
 
